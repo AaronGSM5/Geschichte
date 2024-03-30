dc886a092b6bb41df74df9474c71ba6fafc05a05
AaronGSM5 <aaron.gassmann05@web.de>
2024-03-30 17:45:48 +0100

    final 1.1.0

--- app.js
+++ app.js
@@ -81,35 +81,34 @@ function lightTitle(title) {
   }, 1400);
 }
 
-let touchStartPoint;
-
+//Swiper for Mobile
+let swiperStartPoint;
 slides.addEventListener('touchstart', 
 (e) => {
-  touchStartPoint = e.touches[0].clientX
+  swiperStartPoint = e.touches[0].clientX
 })
 
 slides.addEventListener('touchmove', 
 (e) => {
-    enableTouchEvent = false
+  swipeSlides(e)
+})
+
+function swipeSlides(e) {
+  e.preventDefault()
+  let swiperCurrentPoint = e.touches[0].clientX
+
+  if (swiperStartPoint - swiperCurrentPoint > 100) {
+    const newIndex = activeIndex+1 > 5 ? 0 : activeIndex+1 ;
+    changeSlides(newIndex)
+    swiperStartPoint = 0
+    swiperCurrentPoint = 0
+  }
+
+  if (swiperCurrentPoint - swiperStartPoint > 100) {
     e.preventDefault()
-    let touchCurrentPoint = e.touches[0].clientX
-
-    if (touchStartPoint - touchCurrentPoint > 100) {
-      const newIndex = activeIndex+1 > 5 ? 0 : activeIndex+1 ;
-      changeSlides(newIndex)
-      touchStartPoint = 0
-      touchCurrentPoint = 0
-    }
-  
-    if (touchCurrentPoint - touchStartPoint > 100) {
-      e.preventDefault()
-      const newIndex = activeIndex-1 < 0 ? 5 : activeIndex-1 ;
-      changeSlides(newIndex)
-      touchStartPoint = 0
-      touchCurrentPoint = 0
-    }
-})
-
-slides.addEventListener('touchend', 
-(e) => {
-})
+    const newIndex = activeIndex-1 < 0 ? 5 : activeIndex-1 ;
+    changeSlides(newIndex)
+    swiperStartPoint = 0
+    swiperCurrentPoint = 0
+  }
+}
